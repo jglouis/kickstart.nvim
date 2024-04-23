2475b2a3cc63cc89f1828400dabe96a7eff7dd69
Jean-Guillaume Louis <jguillaume.louis@gmail.com>
2024-04-23 21:47:25 +0200

    Add oil.nvim

--- init.lua
+++ init.lua
@@ -869,6 +869,20 @@ require('lazy').setup({
     'tpope/vim-fugitive',
     opt = {},
   },
+  {
+    'stevearc/oil.nvim',
+    lazy = false,
+    opts = {
+      columns = {
+        'icon',
+        'permissions',
+        'size',
+        'mtime',
+      },
+      skip_confirm_for_simple_edits = true,
+    },
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+  },
 
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
