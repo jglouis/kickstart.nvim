15584d61fdf3da9f439a588df12ad07f6a1c75f5
Jean-Guillaume Louis <jguillaume.louis@gmail.com>
2024-05-13 12:56:21 +0200

    Revert previous commit
    Can't seem to find norg_meta parser when init.lua is being run. Not sure
    to understand why...

--- init.lua
+++ init.lua
@@ -1039,7 +1039,7 @@ require('lazy').setup({
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     opts = {
-      ensure_installed = { 'bash', 'c', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc', 'norg_meta' },
+      ensure_installed = { 'bash', 'c', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
