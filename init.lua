a98962ab57545b2d3b102111879e9a09ad505aea
Jean-Guillaume Louis <jguillaume.louis@gmail.com>
2024-07-08 07:50:05 +0200

    Add some more default TS languages

--- init.lua
+++ init.lua
@@ -1083,7 +1083,7 @@ require('lazy').setup({
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     opts = {
-      ensure_installed = { 'bash', 'c', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc' },
+      ensure_installed = { 'bash', 'c', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc', 'json', 'xml', 'java', 'kotlin', 'rust', 'go', 'javascript' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
